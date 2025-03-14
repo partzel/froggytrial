4e1109bf852f78a67697802e7b44b1e0f039d6e3
partzel <ahmedelmahdibendou2003@gmail.com>
2025-03-14 22:05:58 +0100

    fix: adjust input map

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,9 +12,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if is_on_floor():
 		if direction:
 			animated_sprite.play("run")
